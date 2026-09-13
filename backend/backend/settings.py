e000bfb3e0e8237d3de79c0241c63e6f3a238320
AlexNIvanov <ivanov.driller@yandex.ru>
2026-09-13 19:37:28 +0300

    static updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 # Указываем корневую директорию для сборки статических файлов;
 # в контейнере это будет /app/collected_static
-STATIC_ROOT = BASE_DIR / "collected_static"
+STATIC_ROOT = BASE_DIR / "backend_static"
 # Теперь при вызове команды python manage.py collectstatic
 # Django будет копировать все статические файлы в директорию collected_static
 # Default primary key field type
